db3eca9ed627539a03561082580c28455a5b509a
k-takahashi <takahashi@fullness.ne.jp>
2020-04-28 14:21:37 +0900

    3rd

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ public class Sample{
 	public static void main(String args[]) {
 //		System.out.println("second commit");
 		System.out.println("third push");
+		System.out.println("4th commit");
 	}
 }
