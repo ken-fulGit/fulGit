827e50023547f2e9f3afb91de9befb576bd700e2
k-takahashi <takahashi@fullness.ne.jp>
2020-05-07 09:51:54 +0900

    scanner追加

--- Sample2.java
+++ Sample2.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Sample{
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("third push");
 		//ŽŽ‚µƒuƒ‰ƒ“ƒ`
 		System.out.println("4th commit!!!
