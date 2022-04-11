66b6622ae45968cd3fd81b08edf43aae4dbed7ac
AH-0309\fullness <fnsOP2018003@gmail.com>
2022-04-11 13:27:52 +0900

    test

--- Sample.java
+++ Sample.java
@@ -3,6 +3,6 @@ public class Sample{
 		System.out.println("third push");
 		//試しブランチ
 		System.out.println("4th commit");
-		System.out.println("サンプルプログラム");
+		System.out.println("サンプルプログラムs");
 	}
 }
